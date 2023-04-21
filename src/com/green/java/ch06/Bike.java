04c5f08cb0102762b4c7a44e16cdc659061452cd
LimJeongHo <dlawjdghbn@naver.com>
2023-04-21 14:36:49 +0900

    commit

--- src/com/green/java/ch06/Bike.java
+++ src/com/green/java/ch06/Bike.java
@@ -1,9 +1,22 @@
 package com.green.java.ch06;
 
 public class Bike {
-    static String brand;//클래스 변수 클래스 멤버필드
+    String brand;//클래스 변수 클래스 멤버필드
     String model;//
     int price;
+
+    public Bike(){
+    }
+    public Bike(String brand, String model, int price){
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+    public Bike(Bike bike){
+        this.brand = bike.brand;
+        this.model = bike.model;
+        this.price = bike.price;
+    }
     void drive(){
         System.out.printf("%s브랜드 %s모델",brand,model);
     }
--- src/com/green/java/ch06/BikeTest.java
+++ src/com/green/java/ch06/BikeTest.java
@@ -1,8 +1,18 @@
-package com.green.java.ch06;
+/*package com.green.java.ch06;
+
+import java.util.Scanner;
 
 public class BikeTest {
 
     public static void main(String[] args) {
+        int num = Integer.parseInt("12");
+        Scanner scan = new Scanner(System.in);//<(System.in)스태틱:객체화 안하고 사용 대문자로 사용하는데 .썻으면 스태틱
+        Bike b1 = new Bike("혼다");
+        Bike b2 = new Bike("야마하");
+
+        System.out.println("b1.brand:"+b1.brand);
+        System.out.println("b2.brand:"+b2.brand);
+
 
     }
-}
+}*/
