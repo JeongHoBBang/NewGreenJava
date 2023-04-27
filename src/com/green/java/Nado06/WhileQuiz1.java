063946cdba8061eb3842a49b0dc436974a472037
LimJeongHo <dlawjdghbn@naver.com>
2023-04-27 21:45:36 +0900

    commit

---
+++ src/com/green/java/Nado06/WhileQuiz1.java
@@ -0,0 +1,22 @@
+package com.green.java.Nado06;
+
+import java.util.Scanner;
+
+public class WhileQuiz1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            System.out.println("숫자를 입력 (종료:0)>");
+            int input = scan.nextInt();
+            sum += input;
+            if (input == 0) {
+                break;
+            }
+
+        }System.out.println("합계 :" + sum);
+
+    }
+    }
+
+
---
+++ src/com/green/java/Nado06/WhileStudy1.java
@@ -0,0 +1,16 @@
+package com.green.java.Nado06;
+
+public class WhileStudy1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(++i);
+        }
+//        while(true){
+//
+//            if (멈추고 싶은 조건){
+//                break; 주석한게 베스트!!
+
+    }
+}
+
